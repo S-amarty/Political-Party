e73d5904ee3ff86ab9346a30dac27fda50d154ce
Amar Khedkar <smartynew0@gmail.com>
2023-03-13 14:38:24 +0530

    Initial commit

---
+++ src/main/java/com/example/PoliticalParty/PoliticalPartyApplication.java
@@ -0,0 +1,13 @@
+package com.example.PoliticalParty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoliticalPartyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoliticalPartyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/PoliticalParty/PoliticalPartyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PoliticalParty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoliticalPartyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
